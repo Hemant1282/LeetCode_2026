7f8f73fd23318cd4c92cf26405acbc66ce8371f8
Hemant21082 <hemantshinde2108@gmail.com>
2025-12-28 22:18:44 +0530

    done majority Element leetcode que

---
+++ MajorityElement.java
@@ -0,0 +1,24 @@
+public class MajorityElement{
+
+
+    public static int Majorityel(int arr[]){
+        int candidate = 0;
+        int count = 0;
+        for(int i = 0;i<arr.length;i++){
+            if(count == 0){
+                candidate = arr[i];
+                count += 1;
+            }
+            else if(candidate == arr[i]){
+                count +=1;
+            }
+            else{
+                count -= 1;
+            }
+        }
+        return candidate;
+    }
+    public static void main(String args[]){
+        
+    }
+}

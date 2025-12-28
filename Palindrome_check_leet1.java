19e5df11d8b9d8813aaf9d8376be9ba8d1b119bf
Hemant21082 <hemantshinde2108@gmail.com>
2025-12-28 17:46:12 +0530

    palindrome program

---
+++ Palindrome_check_leet1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Palindrome_check_leet1{
+
+
+    //check for a palindrome number 
+    public static boolean check_palindrome(int num){
+        int reverse = 0;
+        int org_num = num;
+        while(num != 0){
+            int lastdigit = num % 10;
+            reverse = reverse * 10 + lastdigit;
+            num = num/10;
+        }
+
+        if(org_num == reverse){
+            return true;
+        }
+        return false;
+    };
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number to check for palindrome : -- ");
+        int num = sc.nextInt();
+        String result = check_palindrome(num)? "It is a palindrome" : "It is not a palindrome";
+        System.out.println(result);
+    }
+}

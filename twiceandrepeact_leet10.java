d9f8d5451df6214ff7418999964cad64b0d493ae
Hemant21082 <hemantshinde2108@gmail.com>
2025-12-29 18:08:48 +0530

    Twice and repeat leet code que

---
+++ twiceandrepeact_leet10.java
@@ -0,0 +1,42 @@
+public class twiceandrepeact_leet10 {
+
+    public static int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+        int nSquared = n * n;
+        
+        long expectedSum = (long) nSquared * (nSquared + 1) / 2;
+        long expectedSumSquares = (long) nSquared * (nSquared + 1) * (2 * nSquared + 1) / 6;
+        
+        long actualSum = 0, actualSumSquares = 0;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                actualSum += grid[i][j];
+                actualSumSquares += (long) grid[i][j] * grid[i][j];
+            }
+        }
+
+        // a - b
+        long diffSum = actualSum - expectedSum;
+
+        // a² - b²
+        long diffSumSquares = actualSumSquares - expectedSumSquares;
+        
+        // a + b = (a² - b²) / (a - b)
+        long sumAB = diffSumSquares / diffSum;
+        
+        // Now we can find a and b
+        int a = (int) ((sumAB + diffSum) / 2);
+        int b = (int) ((sumAB - diffSum) / 2);
+        
+        return new int[]{a, b};
+    }
+    public static void main(String[] args) {
+        int[][] grid = {{9,1,7},{8,9,2},{3,4,6}};
+        int ans[] = findMissingAndRepeatedValues(grid);
+
+        System.out.println(ans[0]);
+        System.out.println(ans[1]);
+
+    }
+}

4ea75d2b1029fc186a7942642081dbd8cc333fa3
Hemant21082 <hemantshinde2108@gmail.com>
2025-12-29 19:11:14 +0530

    merge two sorted array with two pointer approach

--- Merge_leet11.java
+++ Merge_leet11.java
@@ -10,6 +10,22 @@ public class Merge_leet11 {
         Arrays.sort(nums1);
     };
 
+    //Two pointer approach
+    public static void Merge_two_sorted_arr2(int[] nums1,int m,int[] nums2,int n){
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while(j >= 0){
+            if(i>=0 && nums1[i] > nums2[j]){
+                nums1[k--] = nums1[i--];
+            }
+            else{
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+
     public static void main(String args[]){
         int nums1[] = {1,2,3,0,0,0};
         int m = 3;

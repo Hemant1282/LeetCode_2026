46e2eb9dcb2dbde564bbc211314e4b14e1ccc936
Hemant21082 <hemantshinde2108@gmail.com>
2025-12-30 21:55:14 +0530

    two-d matrix find target

---
+++ Twodmatrixfindtarget_leet14.java
@@ -0,0 +1,25 @@
+public class Twodmatrixfindtarget_leet14 {
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int row = 0;
+        int col = matrix[0].length-1;
+        while(row < m && col >= 0){
+            if(matrix[row][col] == target){
+                return true;
+            }
+            else if(matrix[row][col] < target){
+                row++;
+            }
+            else{
+                col--;
+            };
+        };
+        return false;
+
+    };
+
+    public static void main(String args[]){
+        
+    }
+}
